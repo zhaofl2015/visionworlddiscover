116a89b1e5167d7d449202cfbcadbaf137e20a92
zhaofl2015 <zhaofl2015@163.com>
2017-06-01 19:53:23 +0800

    dynamic

--- pages/fitment/fitment.js
+++ pages/fitment/fitment.js
@@ -9,7 +9,137 @@ Page({
     winWidth: 0,
     winHeight: 0,
     // tab切换  
-    currentTab: 0,
+    currentTab: "0",
+
+    // 导航信息
+    nav_list: [
+      {
+        "title": "第一阶段",
+        "name": "方案设计",
+        "current_tab": "0"
+      },
+      {
+        "title": "第二阶段",
+        "name": "前置订购",
+        "current_tab": "1"
+      },
+      {
+        "title": "第三阶段",
+        "name": "装修施工",
+        "current_tab": "2"
+      },
+      {
+        "title": "第四阶段",
+        "name": "成品安装",
+        "current_tab": "3"
+      },
+      {
+        "title": "第五阶段",
+        "name": "竣工验收",
+        "current_tab": "4"
+      },
+      {
+        "title": "第六阶段",
+        "name": "开荒保洁",
+        "current_tab": "5"
+      },
+      {
+        "title": "第七阶段",
+        "name": "家具软装",
+        "current_tab": "6"
+      },
+      {
+        "title": "第八阶段",
+        "name": "乔迁新居",
+        "current_tab": "7"
+      }
+    ],
+    content_list: [
+      [
+        {
+          "title": "第1步：设计准备",
+          "steps": [
+            {
+              "url": "",
+              "title": "1、找设计师前的功课"
+            },
+            {
+              "url": "",
+              "title": "2、确定装修方式"
+            }
+          ]
+        },
+        {
+          "title": "第2步：签署合同",
+          "steps": [
+            {
+              "url": "",
+              "title": "1、签署设计合同"
+            },
+            {
+              "url": "",
+              "title": "2、签署施工合同"
+            }
+          ]
+        }
+      ],
+      [
+        {
+          "title": "第1步：设计准备",
+          "steps": [
+            {
+              "url": "",
+              "title": "1、找设计师前的功课"
+            },
+            {
+              "url": "",
+              "title": "2、确定装修方式"
+            }
+          ]
+        },
+        {
+          "title": "第2步：签署合同",
+          "steps": [
+            {
+              "url": "",
+              "title": "1、签署设计合同"
+            },
+            {
+              "url": "",
+              "title": "2、签署施工合同"
+            }
+          ]
+        }
+      ],
+      [
+        {
+          "title": "第1步：设计准备",
+          "steps": [
+            {
+              "url": "",
+              "title": "1、找设计师前的功课"
+            },
+            {
+              "url": "",
+              "title": "2、确定装修方式"
+            }
+          ]
+        },
+        {
+          "title": "第2步：签署合同",
+          "steps": [
+            {
+              "url": "",
+              "title": "1、签署设计合同"
+            },
+            {
+              "url": "",
+              "title": "2、签署施工合同"
+            }
+          ]
+        }
+      ]
+    ]
   },
   onLoad: function () {
     var that = this;
